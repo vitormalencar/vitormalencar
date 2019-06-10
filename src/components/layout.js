dbdd6de9f2a1efe8679c18bc89393d6a62111286
vitormalencar <vitor.maia.alencar@gmail.com>
2019-06-10 13:54:09 +0200

    cleaning layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,23 +23,9 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
+      <main>
+        {children}
+      </main>
     )}
   />
 )
