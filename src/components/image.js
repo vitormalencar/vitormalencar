b9eaebdfc6c3f22eb5f1881f848243e1c28dccfe
vitormalencar <vitor.maia.alencar@gmail.com>
2019-06-10 13:57:04 +0200

    updating image component

--- src/components/image.js
+++ src/components/image.js
@@ -17,7 +17,7 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "vitoralencar.webp" }) {
+        placeholderImage: file(relativePath: { eq: "vitoralencar.png" }) {
           childImageSharp {
             fluid(maxWidth: 300) {
               ...GatsbyImageSharpFluid
