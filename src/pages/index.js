0f5c528fcfc03c88714ae62de04c9ddd3ae1b828
vitormalencar <vitor.maia.alencar@gmail.com>
2019-06-10 13:57:14 +0200

    update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
+import data from "../../data.json"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Sidebar from "../components/sidebar"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="vitormalrn" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="col left">
+      <Sidebar {...data} />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="col right">Second Column</div>
   </Layout>
 )
 
