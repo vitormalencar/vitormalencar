dd148fd2ba077f216fb1ba696a37ec56f8a7bdf1
vitormalencar <vitor.maia.alencar@gmail.com>
2020-08-13 21:33:43 +0200

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-undef */
 module.exports = {
-  env: { es6: true },
+  env: { browser: true, es6: true },
   extends: ["eslint:recommended", "plugin:react/recommended"],
-  globals: { Atomics: "readonly", SharedArrayBuffer: "readonly" },
-  parserOptions: { ecmaFeatures: { jsx: true } },
-  plugins: ["react"],
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
   rules: {},
 }
