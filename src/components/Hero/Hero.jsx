d195bd5bdbe17e6e72edfd81c226ac7a0fe64bb9
vitormalencar <vitor.maia.alencar@gmail.com>
2020-09-05 22:47:12 +0200

    updating image size

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,20 +3,20 @@ import Fade from "react-reveal/Fade"
 import AboutImg from "../Image/AboutImg"
 
 export const Hero = ({ about }) => (
-  <div>
-    <div className="pb-40">
-      <AboutImg />
-    </div>
-    <Fade bottom duration={1000} delay={300} distance="0px">
-      <h2 className="text-2xl whitespace-pre font-bold leading-tight lg:text-4xl lg:leading-none text-zenith">
-        {about.title}
-      </h2>
-    </Fade>
-    <div className="leading-loose">
-      <p className="mt-32 whitespace-pre-line">{about.paragraphOne}</p>
-    </div>
-    <button className="md:block hidden rounded bg-purble text-white font-bold mt-40  px-160 py-16">
-      Lets talk
-    </button>
-  </div>
-)
+         <div>
+           <div className="pb-40">
+             <AboutImg />
+           </div>
+           <Fade bottom duration={1000} delay={300} distance="0px">
+             <h2 className="text-2xl whitespace-pre font-bold leading-tight lg:text-4xl lg:leading-none text-zenith">
+               {about.title}
+             </h2>
+           </Fade>
+           <div className="leading-loose">
+             <p className="mt-32 whitespace-pre-line">{about.paragraphOne}</p>
+           </div>
+           <button className="md:block hidden rounded bg-blue-500 hover:bg-red-500 hover:text-white font-bold mt-40 px-160 py-16">
+             Lets talk
+           </button>
+         </div>
+       )
